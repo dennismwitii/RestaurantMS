2c171ec335bc252712d576310842e422d4b20e35
dennismwitii <dennisbmwiti@gmail.com>
2023-04-17 21:31:23 +0300

    first commit

---
+++ application/views/login.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Login</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.7 -->
+  
+  
+  <link rel="stylesheet" href="<?php echo base_url('assets/bower_components/bootstrap/dist/css/bootstrap.min.css') ?>">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="<?php echo base_url('assets/bower_  components/font-awesome/css/font-awesome.min.css') ?>">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="<?php echo base_url('assets/bower_components/Ionicons/css/ionicons.min.css') ?>">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="<?php echo base_url('assets/dist/css/AdminLTE.min.css') ?>">
+  <!-- iCheck -->
+  <link rel="stylesheet" href="<?php echo base_url('assets/plugins/iCheck/square/blue.css') ?>">
+
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+
+  <!-- Google Font -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/webrtc-adapter/3.3.3/adapter.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.1.10/vue.min.js"></script>
+<script type="text/javascript" src="https://rawgit.com/schmich/instascan-builds/master/instascan.min.js"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+</head>
+<body class="hold-transition login-page">
+
+
+<div class="login-box">
+  <!--<div class="icon">
+    <i class="ionicons ion-android-restaurant"></i>
+  </div>-->
+  <div class="login-logo">
+    <a href="#"><b>U S M & O</b> Restaurant</a>
+  </div>
+  <!-- /.login-logo -->
+  <div class="login-box-body">
+    <p class="login-box-msg">Please login to start your session</p>
+
+    <?php echo validation_errors(); ?>  
+
+    <?php if(!empty($errors)) {
+      echo $errors;
+    } ?>
+
+    <form action="<?php echo base_url('auth/login') ?>" method="post">
+      <div class="form-group has-feedback">
+        <input type="email" class="form-control" name="email" id="email" placeholder="Email" autocomplete="off">
+        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+      </div>
+      <div class="form-group has-feedback">
+        <input type="password" class="form-control" name="password" id="password" placeholder="Password" autocomplete="off">
+        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+      </div> 
+      <div class="row">
+        <div class="col-xs-8">
+          <div class="checkbox icheck">
+            <label>
+              <input type="checkbox"> Remember Me
+            </label>
+          </div>
+        </div>
+        <!-- /.col -->
+        <div class="col-xs-4">
+          <button type="submit" class="btn btn-success btn-block btn-flat">Log In</button>
+        </div>
+        <!-- /.col -->
+      </div>
+    </form>
+ <div class="row">
+                <div class="col-md-6">
+                    <video id="preview" width="100%"></video>
+                </div>
+                <div class="col-md-6">
+                    <label>SCAN QR CODE</label>
+                    <input type="text" name="text" id="text" readonyy="" placeholder="scan qrcode" class="form-control">
+                </div>
+            </div>
+  </div>
+  <!-- /.login-box-body -->
+</div>
+<!-- /.login-box -->
+<?php 
+
+if( !session_id() )
+{
+    session_start();
+}
+
+if(@$_SESSION['logged_in'] == true){
+    header("Location: home.php");
+}
+?>
+<link rel="stylesheet" type="text/css" href="style.css">
+<script type="text/javascript" src="./main.js"></script>
+<script type="text/javascript" src="./llqrcode.js"></script>
+
+<div style="display:none" id="result"></div>
+	<div class="selector" id="webcamimg" onclick="setwebcam()" align="left" ></div>
+		<div class="selector" id="qrimg" onclick="setimg()" align="right" ></div>
+			<center id="mainbody"><div id="outdiv"></div></center>
+				<canvas id="qr-canvas" width="800" height="600"></canvas>
+
+<script type="text/javascript">load();</script>
+<script src="./jquery-1.11.2.min.js"></script>
+<!-- jQuery 3 -->
+
+<style>
+  .icon {
+    font-size: 70px;
+    text-align: center;
+  }
+</style>
+
+<script src="<?php echo base_url('assets/bower_components/jquery/dist/jquery.min.js') ?>"></script>
+<!-- Bootstrap 3.3.7 -->
+<script src="<?php echo base_url('assets/bower_components/bootstrap/dist/js/bootstrap.min.js') ?>"></script>
+<!-- iCheck -->
+<script src="<?php echo base_url('assets/plugins/iCheck/icheck.min.js') ?>"></script>
+<script>
+  $(function () {
+    $('input').iCheck({
+      checkboxClass: 'icheckbox_square-blue',
+      radioClass: 'iradio_square-blue',
+      increaseArea: '20%' // optional
+    });
+  });
+</script>
+    <script>
+           let scanner = new Instascan.Scanner({ video: document.getElementById('preview')});
+           Instascan.Camera.getCameras().then(function(cameras){
+               if(cameras.length > 0 ){
+                   scanner.start(cameras[0]);
+               } else{
+                   alert('No cameras found');
+               }
+
+           }).catch(function(e) {
+               console.error(e);
+           });
+
+           scanner.addListener('scan',function(c){
+               document.getElementById('text').value=c;
+           });
+
+        </script>
+</body>
+</html>
---
+++ application/views/templates/footer.php
@@ -0,0 +1,17 @@
+
+  <footer class="main-footer">
+    <div class="pull-right hidden-xs">
+     
+    </div>
+    <strong>&copy; <?php echo date('Y'); ?> |</strong> All rights
+    reserved - UTAMU SMART MENU AND ORDERING
+  </footer>
+
+  <!-- Add the sidebar's background. This div must be placed
+       immediately after the control sidebar -->
+  <div class="control-sidebar-bg"></div>
+</div>
+<!-- ./wrapper -->
+
+</body>
+</html>
